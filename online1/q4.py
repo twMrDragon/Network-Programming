ff222c13cd0c7f88bbbaa3a4c4f2df059f3eaaa8
Joseph laptop <joseph122792@gmail.com>
2023-10-27 15:22:20 +0800

    let q4 auto download

--- online1/q4.py
+++ online1/q4.py
@@ -3,22 +3,16 @@ import zipfile
 import os
 import pandas as pd
 
-# 調整是否下載zip
-IS_DWONLOAD_ZIP = False
-
 # 獲取當前資料夾
 scriptDir = os.path.dirname(__file__)
+zipFilename = os.path.join(scriptDir, "bus.zip")
 
-# zip很大，你忍一下
-# 這裡我就不重新下載了
-zipName = os.path.join(scriptDir, "bus.zip")
-if IS_DWONLOAD_ZIP:
-    url = 'https://data.ntpc.gov.tw/api/datasets/34b402a8-53d9-483d-9406-24a682c2d6dc/csv/zip'
-    # api request 請求下載檔案
-    urllib.request.urlretrieve(url, zipName)
+# api request 請求下載檔案
+url = 'https://data.ntpc.gov.tw/api/datasets/34b402a8-53d9-483d-9406-24a682c2d6dc/csv/zip'
+urllib.request.urlretrieve(url, zipFilename)
 
 # 解壓縮
-f = zipfile.ZipFile(zipName)
+f = zipfile.ZipFile(zipFilename)
 for filename in f.namelist():
     # 解壓縮至當前檔案
     f.extract(filename, scriptDir)
@@ -45,4 +39,5 @@ for index, row in df.head(5).iterrows():
     print(index, row['num'])
 
 # 清理檔案
+os.remove(zipFilename)
 os.remove(csvFilename)
